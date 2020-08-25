741fd309a3e4ee1443dc26aba8f43c4db0f10f20
Rahul <madarahr40@gmail.com>
2020-08-25 03:56:17 -0500

    commit to repo branch

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -2,14 +2,32 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 
-// We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+// Refer Leaflet Quick Start
+// Under markers, circles, polygons
+//  Add a marker to the map for Los Angeles, California.
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+// Replacing the marker with a circle with radius of 100 miles to represent Los Angeles
+// L.circle([34.0522, -118.2437], {
+// radius: 100,
+// color: "black",
+// fillcolor: '#ffffa1'
+// }).addTo(map);
+
+ // Alternatively to above, a circle marker is used
+ L.circleMarker([34.0522, -118.2437], {
+	 radius: 100,
+	 color: "black",
+	 fillcolor: '#ffffa1'
+ 	}).addTo(map);
+
+	 // We create the tile layer that will be the background of our map.
+	let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
 	accessToken: API_KEY
-});
-
+	});
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);

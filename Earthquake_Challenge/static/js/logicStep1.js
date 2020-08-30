5b388d5706d31097d87611b8b2e7536fd298907f
Rahul <madarahr40@gmail.com>
2020-08-30 02:22:40 -0500

    adding Earthquake_Challenge to branch

---
+++ Earthquake_Challenge/static/js/logicStep1.js
@@ -0,0 +1,62 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+
+// We create the tile layer that will be the background of our map.
+	let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+	accessToken: API_KEY
+	});
+
+	// We create the tile view tile layer that will be a background for our map.
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+	attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+		maxZoom: 18,
+		accessToken: API_KEY
+	});
+
+// We create the tile view tile layer that will be a background for our map.
+// let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+// 	maxZoom: 18,
+// 	accessToken: API_KEY
+//});
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+	Street: streets,
+	"Satellite": satelliteStreets
+  };
+
+  // Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+	center: [39.5, -98.5],
+	zoom: 3,
+	layers: [streets]
+});
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+
+// Retrieve the earthquake GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJson(data).addTo(map);
+});
+
+// mapbox.streets-v10
+// mapbox.light-v10
+// mapbox.dark=v10
+// mapbox.satellite
+// mapbox.streets-satellite-v11
+// mapbox.wheatpaste
+// mapbox.streets-basic
+// mapbox.comic
+// mapbox.outdoors
+// mapbox.run-bike-hike
+// mapbox.pencil
+// mapbox.pirates
+// mapbox.emerald
+// mapbox.high-contrast
